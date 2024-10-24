20ea9a93a91152cc00bd170ad98b93eddce389c3
baroordp <169070181+baroordp@users.noreply.github.com>
2024-10-24 20:15:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,6 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+JqkfroPkJaVhMm
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002226519917').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
+SECONDDB_URI = environ.get('SECONDDB_URI', "")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Db6:Db6@cluster0.8dtyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
